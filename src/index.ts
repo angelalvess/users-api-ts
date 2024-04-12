48a9fdcc0607270d5976d631d0822ed3812e02df
angelalvess <angeltrashangel@gmail.com>
2024-04-12 20:52:47 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello, World!, Angel here');
+import express from 'express';
+import { config } from 'dotenv';
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World, API is running');
+});
+
+app.listen(port, () => {
+  console.log(`Server on air, running on port ${port}`);
+});
